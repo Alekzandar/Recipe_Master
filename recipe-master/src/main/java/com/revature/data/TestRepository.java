28835ec9bdf482ef3ddbf847f1588d69ec532515
tnawshin <tawbin.nawshin@baruchmail.cuny.edu>
2019-04-10 10:43:54 -0400

    more code on recipe

--- recipe-master/src/main/java/com/revature/data/TestRepository.java
+++ recipe-master/src/main/java/com/revature/data/TestRepository.java
@@ -1,7 +1,9 @@
-package com.revature.model;
+package com.revature.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.revature.model.TestEntity;
 
 /*
  * Specify queries to DataBase with Spring Data here.
