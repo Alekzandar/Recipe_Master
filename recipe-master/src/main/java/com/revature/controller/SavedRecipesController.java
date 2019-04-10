6b755011af0cb9a7821ca293625f6d6b30a5fe4b
tnawshin <tawbin.nawshin@baruchmail.cuny.edu>
2019-04-10 16:32:59 -0400

    update for controllers

--- recipe-master/src/main/java/com/revature/controller/SavedRecipesController.java
+++ recipe-master/src/main/java/com/revature/controller/SavedRecipesController.java
@@ -1,5 +1,22 @@
 package com.revature.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.data.RecipeRepository;
+import com.revature.data.UserRepository;
+
+@RestController
+@RequestMapping("/savedRecipe")
 public class SavedRecipesController {
+	
+	@Autowired
+	RecipeRepository recipeRepo;
+	
+	@Autowired
+	UserRepository repo;
+	
+	//saving recipeId by userId
 
 }
--- recipe-master/src/main/java/com/revature/controller/SharedRecipesController.java
+++ recipe-master/src/main/java/com/revature/controller/SharedRecipesController.java
@@ -1,5 +1,23 @@
 package com.revature.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.data.RecipeRepository;
+import com.revature.data.UserRepository;
+
+@RestController
+@RequestMapping("/sharedRecipe")
 public class SharedRecipesController {
+	
+	@Autowired
+	RecipeRepository recipeRepo;
+	
+	@Autowired
+	UserRepository repo;
+	
+	//each userID can share 5 recipes maximum
+
 
 }
