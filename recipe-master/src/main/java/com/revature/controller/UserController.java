3eceb34b3fbad634d3b3b1bb667664cd196b0f17
Aleksandar Antonov <alekzandar@live.com>
2019-04-12 22:23:03 -0400

    Get by Username

--- recipe-master/src/main/java/com/revature/controller/UserController.java
+++ recipe-master/src/main/java/com/revature/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.data.UserRepository;
 import com.revature.model.User;
 
+import revature.com.service.UserService;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	UserService service;
+	
 	@Autowired
 	UserRepository repo;
 
@@ -29,5 +36,19 @@ public class UserController {
 	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<User>> getAll() {
 		return new ResponseEntity<List<User>>(repo.findAll(), HttpStatus.OK);
+	}
+	
+	//GET /users/id
+	//get user by id
+	@RequestMapping(value="/{username}")
+	public ResponseEntity<User> getByUsername(@PathVariable String username) {
+		User u =  repo.findByUsernameIgnoreCase(username);
+		if(u == null) {
+			//no user is found.. Http status of no content 
+			return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+		}else {
+			//return user with status OK
+			return new ResponseEntity<User>(u, HttpStatus.OK);
+		}
 	}
 }
